4dc6afe1dc2630d984b0fee0fa9cc432d602fb81
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2024-10-07 17:02:13 -0300

    docs: correcao do commit anterior

--- Capitulo 7 - Introdução à POO/Exercicios/src/exercicioFixacao/CurrencyConverter.java
+++
@@ -1,9 +0,0 @@
-package exercicioFixacao;
-
-public class CurrencyConverter {
-	private static final double IOF = 0.06;
-	
-	public static double dolarConverter(double price, double quantity) {
-		return price*quantity*(1+IOF);
-	}
-}
